86b82ebc3ed2f0fd49be07ec234811b0f61d2598
Sharon Funke <sharon.funke@nzz.ch>
2017-03-15 16:27:25 +0100

    Add comments in editor config

--- config/editorConfig.js
+++ config/editorConfig.js
@@ -1,7 +1,7 @@
 const Confidence = require('confidence');
 
 const editorConfig = {
-  languages: [
+  languages: [ // an array of languages, if more that one is configured, Q editor will show a language switcher
     {
       key: 'de',
       label: 'de'
@@ -11,19 +11,27 @@ const editorConfig = {
       label: 'en'
     }
   ],
-  departments: [
+  departments: [ // every item is assigned to one department. These are used for the filtering on item overview.
     "Storytelling"
   ],
-  lowNewItems: {
-    threshold: 21,
-    days: 7
+  lowNewItems: {   // if there are not enough graphics, M will appear instead of Q. This is used to configure 'not enough'.
+    threshold: 21, // less than that many new items..
+    days: 7        // in the last days are considered 'not enough'
   },
   itemList: {
-    itemsPerLoad: 18
+    itemsPerLoad: 18 // how many items to load per step (load more) on item overview
   },
-  help: {
-    intro: 'This help text is part of config/editorConfig in Q server demo. Just change it according to your needs. An introductory text can be defined as well as some detailed information in FAQ style.',
-    faq: [
+  schemaEditor: {
+    latLngLayerConfig: { // if you use schema-editor-lat-lng this is the layer config for Leaflet
+      url: 'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png',
+      config: {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+      }
+    }
+  },
+  help: { // configuration for the help dialog. this has no localization support for now :-(
+    intro: 'This help text is part of config/editorConfig in Q server demo. Just change it according to your needs. An introductory text can be defined as well as some detailed information in FAQ style.', // shown on top of the dialog
+    faq: [ // a list of questions and answers shown below the intro
       {
         question: 'What is Q?',
         answer: 'Q is a toolbox that allows editors and reporters to create simple graphics and interactive elements for their articles or other forms of online content.'
@@ -34,11 +42,16 @@ const editorConfig = {
       }
     ]
   },
-  eastereggs: {
+  stylesheets: [ // a list of stylesheets to load in addition to the default styles. Use this to load your theme if you do not like our design
+    {
+      url: ''
+    },
+  ],
+  eastereggs: { // there are some eastereggs in Q. provide the urls to the soundfiles here. We do not distribute them because we do not have the copyright for the tunes we use at NZZ.
     sounds: {
-      m: 'https://storytelling.nzz.ch/m.mp3',
-      q: 'https://storytelling.nzz.ch/q.mp3',
-      bondTheme: 'https://storytelling.nzz.ch/bond-theme.mp3'
+      m: '', // played if M is visible and user hovers her
+      q: '', // played/paused if user types Shift+Q
+      bondTheme: '' // played if user types Shift+0 Shift+0 Shift+7
     }
   }
 }
